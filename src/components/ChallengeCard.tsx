dc13aa494f7db26761113b0bc3194822490d0ee9
Andre Ortiz <andreortiz82@gmail.com>
2023-06-03 10:28:05 -0500

    added confetti

--- src/components/ChallengeCard.tsx
+++ src/components/ChallengeCard.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { DiceThree } from "phosphor-react";
 import { HowToPlay } from "./HowToPlay";
-import _, { isNull } from "lodash";
+import _ from "lodash";
+import Confetti from "react-confetti";
 
 interface ChallengeCardProps {
   players: any[];
@@ -13,6 +14,7 @@ export const ChallengeCard = (props: ChallengeCardProps) => {
   const { players, skills, challenges } = props;
   const tempRoundDescription = "How to play...";
 
+  const [party, setParty] = useState(false);
   const [skillaborators, setSkillaborators] = useState(null);
   const [roundChallenge, setRoundChallenge] = useState(null);
   const [roundSkill, setRoundSkill] = useState(null);
@@ -21,11 +23,12 @@ export const ChallengeCard = (props: ChallengeCardProps) => {
 
   // useEffect(() => {}, []);
 
-  const chooseNames = () => {
+  const rollDice = () => {
     const randomPlayers = _.sampleSize(players, 3);
     const randomChallenge = _.sampleSize(challenges, 1);
     const randomSkill = _.sampleSize(skills, 1);
 
+    setParty(true);
     setSkillaborators(randomPlayers);
     setRoundChallenge(randomChallenge);
     setRoundSkill(randomSkill);
@@ -37,6 +40,23 @@ export const ChallengeCard = (props: ChallengeCardProps) => {
     setRoundChallenge(null);
     setRoundSkill(null);
     setRoundDescription(tempRoundDescription);
+    setParty(false);
+  };
+
+  const runParty = () => {
+    return (
+      <Confetti
+        onConfettiComplete={(confetti) => {
+          confetti?.reset();
+          setParty(false);
+        }}
+        width={window.innerWidth}
+        run={party}
+        height={window.innerHeight}
+        numberOfPieces={200}
+        recycle={false}
+      />
+    );
   };
 
   return (
@@ -44,10 +64,16 @@ export const ChallengeCard = (props: ChallengeCardProps) => {
       <header className="mb-5 flex gap-6 items-center">
         <h1 className="text-2xl font-bold">Skillaboration!</h1>
         <button
-          className="flex gap-2 items-center text-lg px-4 py-2 cursor-pointer rounded-md bg-green-300 hover:bg-green-400 ease-in-out duration-200"
-          onClick={() => chooseNames()}
+          disabled={party}
+          className={`${
+            party !== true ? "" : "animate-pulse cursor-not-allowed"
+          } flex gap-2 items-center text-lg px-4 py-2 cursor-pointer rounded-md bg-green-300 hover:bg-green-400 ease-in-out duration-200`}
+          onClick={() => party !== true && rollDice()}
         >
-          <DiceThree className="text-2xl" /> <span>Roll to play</span>
+          <DiceThree
+            className={`${party !== true ? "" : "animate-spin"} text-2xl`}
+          />{" "}
+          <span>{party !== true ? "Roll" : "Rolling..."}</span>
         </button>
 
         <button
@@ -61,6 +87,7 @@ export const ChallengeCard = (props: ChallengeCardProps) => {
       <section>
         {skillaborators?.length > 0 ? (
           <>
+            {runParty()}
             <header className="mb-5">
               <h1 className="text-8xl font-bold">{roundDescription}</h1>
             </header>
